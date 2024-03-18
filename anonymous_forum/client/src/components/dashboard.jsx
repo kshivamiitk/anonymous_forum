b2abaa8effabca82266e38f2cfba0c7887e95ac5
kshivamiitk <134162452+kshivamiitk@users.noreply.github.com>
2024-03-18 16:50:42 +0530

    Add files via upload

---
+++ anonymous_forum/client/src/components/dashboard.jsx
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Pagination from "./common/pagination";
+import ListGroup from "./listgroup";
+import Posts from "./posts";
+import { paginate } from "../utils/paginate";
+import { api } from "../config.js";
+import http from "../services/httpService";
+import Jumotron from "./common/jumbotron";
+
+class Dashboard extends Component {
+  state = {
+    allposts: [],
+    currentPage: 1,
+    pageSize: 4,
+    tags: [],
+    selectedTag: { _id: "1", name: "All Posts" },
+  };
+  async componentDidMount() {
+    try {
+      const { data: allposts } = await http.get(api.postsEndPoint);
+      console.log("data", allposts); // Corrected variable name from "data" to "allposts"
+  
+      const { data: tags } = await http.get(api.tagsEndPoint);
+  
+      // Check if allposts is empty
+      if (!allposts || allposts.length === 0) {
+        throw new Error("No posts found.");
+      }
+  
+      this.setState({
+        allposts: [...allposts],
+        tags: [
+          {
+            _id: "1",
+            name: "All Posts",
+          },
+          ...tags,
+        ],
+      });
+    } catch (error) {
+      // Handle the error gracefully
+      console.error("Error fetching data:", error);
+      // Optionally, you can set a default state or display a message to the user
+      this.setState({
+        allposts: [],
+        tags: [],
+        error: "Failed to fetch data. Please try again later.",
+      });
+    }
+  }
+  
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+  handlePostDelete = (post) => {};
+  handleTagSelect = (tag) => {
+    this.setState({ selectedTag: tag, currentPage: 1 });
+  };
+  getPosts() {
+    const { allposts, selectedTag } = this.state;
+    const filtered = [];
+    for (let i in allposts) {
+      const post = allposts[i];
+      const { tags } = post;
+      for (let j in tags) {
+        if (tags[j].name === selectedTag.name) {
+          filtered.push(post);
+          break;
+        }
+      }
+    }
+    console.log(filtered);
+    return filtered;
+  }
+  render() {
+    const { user } = this.props;
+    const { allposts, pageSize, currentPage, tags, selectedTag } = this.state;
+    const filtered = selectedTag._id === "1" ? allposts : this.getPosts();
+    const posts = paginate(filtered, currentPage, pageSize);
+    if (allposts.length === 0)
+      return <p>There are no posts in the database!</p>;
+    return (
+      <React.Fragment>
+        <Jumotron />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="d-flex w-100 justify-content-between m-3">
+                Showing {filtered.length} posts.
+                { (
+                  <Link to="/new-post">
+                    <button
+                      type="button"
+                      class="btn btn-success"
+                      style={{ marginBottom: 20 }}
+                    >
+                      New Post
+                    </button>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-9">
+              <Posts posts={posts} onDelete={this.handlePostDelete} />
+            </div>
+            <div className="col-3">
+              <ListGroup
+                items={tags}
+                selectedTag={this.state.selectedTag}
+                onTagSelect={this.handleTagSelect}
+              />
+            </div>
+            <Pagination
+              itemCount={filtered.length}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ anonymous_forum/client/src/config.js
@@ -0,0 +1,6 @@
+export const api = {  
+  tagsEndPoint: 'http://localhost:9999/tags/',
+  usersEndPoint: 'http://localhost:9999/users/',
+  postsEndPoint: 'http://localhost:9999/posts/',
+  repliesEndPoint: 'http://localhost:9999/reply/',
+}
